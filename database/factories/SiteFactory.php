021ffbe0eb2c2ee4e146ff9e4fa3d6bf80ce34b6
Liam Thursfield <liam.thursfield@nexuspoint.co.uk>
2020-02-15 15:28:11 +0000

    Create SiteFactory

---
+++ database/factories/SiteFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Site;
+use Faker\Generator as Faker;
+
+$factory->define(Site::class, function (Faker $faker) {
+    return [
+        'git_url' => $faker->url,
+        'ssh_key_path' => '.ssh/' . $faker->word(),
+        'site_directory' => '/sites/' . $faker->word(),
+        'current_release_directory' => $faker->word(),
+        'releases_directory' => $faker->word(),
+        'persistent_directory' => $faker->word(),
+        'linked_files' => [
+            [
+                'from' => $faker->word(),
+                'to' => $faker-> word()
+            ],
+            [
+                'from' => $faker->word(),
+                'to' => $faker-> word()
+            ],
+            [
+                'from' => $faker->word(),
+                'to' => $faker-> word()
+            ]
+        ],
+        'pre_activation_script' => [
+            [
+                'active' => $faker->boolean(),
+                'command' => $faker->word(),
+            ],
+            [
+                'active' => $faker->boolean(),
+                'command' => $faker->word(),
+            ],
+            [
+                'active' => $faker->boolean(),
+                'command' => $faker->word(),
+            ]
+        ],
+        'post_activation_script' => [
+            [
+                'active' => $faker->boolean(),
+                'command' => $faker->word(),
+            ],
+            [
+                'active' => $faker->boolean(),
+                'command' => $faker->word(),
+            ],
+            [
+                'active' => $faker->boolean(),
+                'command' => $faker->word(),
+            ]
+        ],
+    ];
+});

f29245bde751f31e1e715c26320b7014e392cee0
Liam Thursfield <liam.thursfield@nexuspoint.co.uk>
2020-03-21 22:16:51 +0000

    Footer update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -35,9 +35,12 @@
 
 
         <footer class="mt-auto">
-            <div class="bg-gray-900 border-gray-700 border-t-2 mt-16 py-8 text-white">
+            <div class="bg-gray-900 border-gray-800 border-t-2 mt-16 py-8 text-gray-600">
                 <div class="container mx-auto">
-                    <p class="text-center">&copy; 2020 Liam Thursfield</p>
+                    <p class="text-center">
+                        &copy; 2020
+                        <span class="text-green-800">Liam Thursfield</span>
+                    </p>
                 </div>
             </div>
         </footer>

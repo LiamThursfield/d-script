9fa6edb932bce34525be4cdd8a915ef4c54d7535
Liam Thursfield <liam.thursfield@nexuspoint.co.uk>
2020-03-22 19:23:04 +0000

    Add meta theme-color

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -5,6 +5,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#1A202C">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
